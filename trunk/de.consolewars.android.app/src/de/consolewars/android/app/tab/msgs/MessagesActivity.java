6a5e983f3776dc98346738ba67762f8fee12a4c8
w4yne1337@gmail.com <w4yne1337@gmail.com@01ab0fe4-f6f9-ce30-f133-f1104d6b77f6>
2011-08-17 22:13:46 +0000

    TableLayout durch ListView ersetzt und ListSelector hinzugefügt (für Messages)

--- trunk/de.consolewars.android.app/src/de/consolewars/android/app/tab/msgs/MessagesActivity.java
+++ trunk/de.consolewars.android.app/src/de/consolewars/android/app/tab/msgs/MessagesActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import roboguice.activity.RoboActivity;
 import android.app.ActivityGroup;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -19,13 +20,12 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.google.inject.Inject;
 
-import de.consolewars.android.app.CWApplication;
 import de.consolewars.android.app.CWManager;
 import de.consolewars.android.app.Filter;
 import de.consolewars.android.app.R;
@@ -57,20 +57,13 @@ import de.consolewars.api.exception.ConsolewarsAPIException;
 public class MessagesActivity extends RoboActivity {
 
 	@Inject
-	private CWApplication cwApplication;
-	@Inject
 	private CWManager cwManager;
 	@Inject
 	private ViewUtility viewUtility;
 
 	private List<Message> msgs = new ArrayList<Message>();
 
-	// remember last selected table row to draw the background
-	private View selectedRow;
-
 	private Filter currentFilter = Filter.MSGS_INBOX;
-
-	private StyleSpannableStringBuilder styleStringBuilder;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -84,7 +77,7 @@ public class MessagesActivity extends RoboActivity {
 	 * 
 	 * @author Alexander Dridiger
 	 */
-	public class BuildMessagesAsyncTask extends AsyncTask<Void, Integer, List<View>> {
+	public class BuildMessagesAsyncTask extends AsyncTask<Void, Integer, List<Message>> {
 
 		@Override
 		protected void onPreExecute() {
@@ -95,79 +88,142 @@ public class MessagesActivity extends RoboActivity {
 		}
 
 		@Override
-		protected List<View> doInBackground(Void... params) {
+		protected List<Message> doInBackground(Void... params) {
 			try {
-				setCurrentMessages();
+				msgs = cwManager.getMessages(currentFilter, 10);
 			} catch (ConsolewarsAPIException e) {
 				e.printStackTrace();
 				Log.e(getString(R.string.exc_auth_tag), e.getMessage(), e);
-				return new ArrayList<View>();
+				return new ArrayList<Message>();
 			}
-			return createMsgsRows();
+			return msgs;
 		}
 
 		@Override
-		protected void onPostExecute(List<View> result) {
+		protected void onPostExecute(List<Message> result) {
 			// sets the messages view for this Activity
 			ViewGroup msgs_layout = (ViewGroup) LayoutInflater.from(MessagesActivity.this.getParent()).inflate(
 					R.layout.msgs_layout, null);
 			initFilter(msgs_layout);
 			initRefreshBttn(msgs_layout);
 			initSendMsgBttn(msgs_layout);
-			TableLayout msgsTable = (TableLayout) msgs_layout.findViewById(R.id.msgs_table);
-			for (View row : result) {
-				msgsTable.addView(row);
-			}
+			ListView msgsListView = (ListView) msgs_layout.findViewById(R.id.msgs_list_view);
+			msgsListView.setAdapter(new MessageRowAdapter(MessagesActivity.this, result));
 			setContentView(msgs_layout);
 		}
 
 		/**
-		 * Sets the newest messages of the logged in user.
+		 * Filter ui and logic for filtering news.
 		 * 
-		 * @throws ConsolewarsAPIException
+		 * @param parentView
+		 *            to find the inflated view elements.
 		 */
-		private void setCurrentMessages() throws ConsolewarsAPIException {
-			msgs = cwManager.getMessages(currentFilter, 10);
+		private void initFilter(final View parentView) {
+
+			Spinner spinner = (Spinner) parentView.findViewById(R.id.msg_filter_spinner);
+			ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MessagesActivity.this,
+					R.array.msgs_filter_options, android.R.layout.simple_spinner_item);
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			spinner.setAdapter(adapter);
+			spinner.setSelection(currentFilter.getPosition());
+			spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+				public void onItemSelected(AdapterView<?> aView, View view, int position, long id) {
+					Filter selected;
+					if (position == Filter.MSGS_OUTBOX.getPosition()) {
+						selected = Filter.MSGS_OUTBOX;
+					} else {
+						selected = Filter.MSGS_INBOX;
+					}
+					if (!currentFilter.equals(selected)) {
+						currentFilter = selected;
+						new BuildMessagesAsyncTask().execute();
+					}
+				}
+
+				public void onNothingSelected(AdapterView<?> arg0) {
+				}
+			});
 		}
 
-		/**
-		 * Create the ui for displaying messages in a table.
-		 */
-		private List<View> createMsgsRows() {
-			// create table based on current messages
-			View msgsView = LayoutInflater.from(MessagesActivity.this.getParent()).inflate(R.layout.msgs_layout, null);
-			TableLayout msgsTable = (TableLayout) msgsView.findViewById(R.id.msgs_table);
+		private void initRefreshBttn(final View parentView) {
+			Button refresh = (Button) parentView.findViewById(R.id.msg_bttn_refresh);
+			refresh.setOnClickListener(new OnClickListener() {
 
-			styleStringBuilder = new StyleSpannableStringBuilder();
+				public void onClick(View view) {
+					new BuildMessagesAsyncTask().execute();
+				}
+			});
+		}
 
-			List<View> rows = new ArrayList<View>();
+		private void initSendMsgBttn(final View parentView) {
+			Button new_bttn = (Button) parentView.findViewById(R.id.msg_bttn_new_mssg);
+			new_bttn.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View arg0) {
+					Intent newMsgIntent = new Intent(MessagesActivity.this, MessageWriterActivity.class);
 
-			for (final Message msg : msgs) {
-				// get the table row by an inflater and set the needed
-				// information
-				final View tableRow = getLayoutInflater().inflate(R.layout.msgs_row_layout, msgsTable, false);
+					View view = ((ActivityGroup) getParent())
+							.getLocalActivityManager()
+							.startActivity(MessageWriterActivity.class.getSimpleName(),
+									newMsgIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)).getDecorView();
+					// replace the view
+					((MessagesActivityGroup) getParent()).replaceView(view);
+				}
+			});
+		}
+	}
 
-				tableRow.setId(msg.getId());
-				tableRow.setOnClickListener(new View.OnClickListener() {
+	public class MessageRowAdapter extends ArrayAdapter<Message> {
 
-					public void onClick(View v) {
-						if (selectedRow != null) {
-							selectedRow.setBackgroundDrawable(getResources().getDrawable(R.drawable.table_cell_bg));
-						}
-						tableRow.setBackgroundDrawable(getResources().getDrawable(R.drawable.table_cell_bg_selected));
-						selectedRow = tableRow;
-						getSingleMessage(msg.getMessage());
-					}
-				});
-				((ImageView) tableRow.findViewById(R.id.msgs_row_read_icon)).setImageResource(createMessageIcon(msg
-						.isMessageread()));
-				((TextView) tableRow.findViewById(R.id.msgs_row_title)).setText(createTitle(msg.getTitle()));
-				((TextView) tableRow.findViewById(R.id.msgs_row_date)).setText(createDate(msg.getUnixtime() * 1000L));
-				((TextView) tableRow.findViewById(R.id.msgs_row_author)).setText(createFromUser(msg.getFromusername()));
-				rows.add(tableRow);
+		private LayoutInflater mInflater;
+		private List<Message> rows;
+
+		public MessageRowAdapter(Context context, List<Message> rows) {
+			super(getApplicationContext(), R.layout.msgs_row_layout, rows);
+			mInflater = LayoutInflater.from(context);
+			this.rows = rows;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			final Message row = rows.get(position);
+
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.msgs_row_layout, null);
+				holder = new ViewHolder();
+				holder.icon = (ImageView) convertView.findViewById(R.id.msgs_row_read_icon);
+				holder.title = (TextView) convertView.findViewById(R.id.msgs_row_title);
+				holder.date = (TextView) convertView.findViewById(R.id.msgs_row_date);
+				holder.author = (TextView) convertView.findViewById(R.id.msgs_row_author);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
 			}
-			styleStringBuilder = null;
-			return rows;
+
+			holder.icon.setImageResource(createMessageIcon(row.isMessageread()));
+			holder.title.setText(createTitle(row.getTitle()));
+			holder.date.setText(createDate(row.getUnixtime() * 1000L));
+			holder.author.setText(createFromUser(row.getFromusername()));
+
+			convertView.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View view) {
+					getSingleMessage(row.getMessage());
+				}
+			});
+
+			return convertView;
+		}
+
+		class ViewHolder {
+			ImageView icon;
+			TextView title;
+			TextView date;
+			TextView author;
 		}
 
 		/**
@@ -233,80 +289,13 @@ public class MessagesActivity extends RoboActivity {
 			if (author == null || author.matches("")) {
 				author = getString(R.string.news_author_unknown);
 			}
+			StyleSpannableStringBuilder styleStringBuilder = new StyleSpannableStringBuilder();
 			styleStringBuilder.clear();
 			styleStringBuilder.appendWithStyle(new ForegroundColorSpan(0xFF007711), getString(R.string.news_author_by));
 			styleStringBuilder.append(" ");
 			styleStringBuilder.appendWithStyle(new ForegroundColorSpan(0xFF009933), author);
 
 			return styleStringBuilder;
-		}
-
-		/**
-		 * Filter ui and logic for filtering news.
-		 * 
-		 * @param parentView
-		 *            to find the inflated view elements.
-		 */
-		private void initFilter(final View parentView) {
-
-			Spinner spinner = (Spinner) parentView.findViewById(R.id.msg_filter_spinner);
-			ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MessagesActivity.this,
-					R.array.msgs_filter_options, android.R.layout.simple_spinner_item);
-			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			spinner.setAdapter(adapter);
-			spinner.setSelection(currentFilter.getPosition());
-			spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-				public void onItemSelected(AdapterView<?> aView, View view, int position, long id) {
-					Filter selected;
-					if (position == Filter.MSGS_OUTBOX.getPosition()) {
-						selected = Filter.MSGS_OUTBOX;
-					} else {
-						selected = Filter.MSGS_INBOX;
-					}
-					if (!currentFilter.equals(selected)) {
-						currentFilter = selected;
-						removeAllMessages(parentView);
-						new BuildMessagesAsyncTask().execute();
-					}
-				}
-
-				public void onNothingSelected(AdapterView<?> arg0) {
-				}
-			});
-		}
-
-		private void initRefreshBttn(final View parentView) {
-			Button refresh = (Button) parentView.findViewById(R.id.msg_bttn_refresh);
-			refresh.setOnClickListener(new OnClickListener() {
-
-				public void onClick(View arg0) {
-					removeAllMessages(parentView);
-					new BuildMessagesAsyncTask().execute();
-				}
-			});
-		}
-
-		private void initSendMsgBttn(final View parentView) {
-			Button new_bttn = (Button) parentView.findViewById(R.id.msg_bttn_new_mssg);
-			new_bttn.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0) {
-					Intent newMsgIntent = new Intent(MessagesActivity.this, MessageWriterActivity.class);
-
-					View view = ((ActivityGroup) getParent())
-							.getLocalActivityManager()
-							.startActivity(MessageWriterActivity.class.getSimpleName(),
-									newMsgIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)).getDecorView();
-					// replace the view
-					((MessagesActivityGroup) getParent()).replaceView(view);
-				}
-			});
-		}
-
-		private void removeAllMessages(View parentView) {
-			TableLayout msgsTable = (TableLayout) parentView.findViewById(R.id.msgs_table);
-			msgsTable.removeAllViews();
 		}
 	}
 }
