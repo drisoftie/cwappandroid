f30cf609c51b9b3adcc45e5f2f79ab9a6b4e925f
alexander.dridiger@gmail.com <alexander.dridiger@gmail.com@01ab0fe4-f6f9-ce30-f133-f1104d6b77f6>
2011-08-20 15:55:50 +0000

    Blogs zurück auf TableLayout, ListView ist zu inperformant. (Update issue 13)

--- de.consolewars.android.app/src/de/consolewars/android/app/util/ViewUtility.java
+++ de.consolewars.android.app/src/de/consolewars/android/app/util/ViewUtility.java
@@ -36,23 +36,69 @@ public class ViewUtility {
 		return progress_layout;
 	}
 
+	/**
+	 * Returns a user icon as a Bitmap with the given size (longest edge).
+	 * 
+	 * @param userId
+	 *            to get the icon from
+	 * @param iconSize
+	 *            longest edge of the returned icon
+	 * @return
+	 */
+	public Bitmap getUserIcon(int userId, int iconSize) {
+		return getBitmap(context.getString(R.string.userpic_url, userId, iconSize));
+	}
+
+	/**
+	 * Sets a user icon to the given {@link ImageView}.
+	 * 
+	 * @param view
+	 * @param userId
+	 *            to get the icon from
+	 * @param iconSize
+	 *            longest edge of the icon
+	 */
 	public void setUserIcon(ImageView view, int userId, int iconSize) {
 		setIcon(view, context.getString(R.string.userpic_url, userId, iconSize));
 	}
 
+	/**
+	 * Sets a category icon to the given {@link ImageView}.
+	 * 
+	 * @param view
+	 * @param categoryShort
+	 *            name of the category to get the icon from
+	 */
 	public void setCategoryIcon(ImageView view, String categoryShort) {
 		setIcon(view, context.getString(R.string.catpic_url, categoryShort));
 	}
 
-	private void setIcon(ImageView view, String url) {
+	/**
+	 * Downloads a picture from the given url and returns it as a Bitmap.
+	 * 
+	 * @param url
+	 * @return
+	 */
+	private Bitmap getBitmap(String url) {
+		Bitmap bitmap = null;
 		try {
 			URL newurl = new URL(url);
-			Bitmap mIcon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
-			view.setImageBitmap(mIcon_val);
+			bitmap = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return bitmap;
+	}
+
+	/**
+	 * Sets an icon to the given {@link ImageView}.
+	 * 
+	 * @param view
+	 * @param url
+	 */
+	private void setIcon(ImageView view, String url) {
+		view.setImageBitmap(getBitmap(url));
 	}
 }
